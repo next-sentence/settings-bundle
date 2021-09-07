fc3d6375789b739953522759a251c6878931ca4c
Petru Binzari <petru@fyb.ro>
2021-09-07 12:02:36 +0300

    fix container

--- src/LwcSettingsBundle.php
+++ src/LwcSettingsBundle.php
@@ -4,36 +4,12 @@ declare(strict_types=1);
 
 namespace Lwc\SettingsBundle;
 
-use LogicException;
 use Lwc\SettingsBundle\DependencyInjection\InstantiateSettingsPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class LwcSettingsBundle extends Bundle
 {
-    /**
-     * Returns the plugin's container extension.
-     *
-     * @throws LogicException
-     *
-     * @return ExtensionInterface|null The container extension
-     */
-    public function getContainerExtension(): ?ExtensionInterface
-    {
-        if (null === $this->containerExtension) {
-            $this->containerExtension = false;
-            $extension = $this->createContainerExtension();
-            if (null !== $extension) {
-                $this->containerExtension = $extension;
-            }
-        }
-
-        return $this->containerExtension instanceof ExtensionInterface
-            ? $this->containerExtension
-            : null;
-    }
-
     /**
      * @param ContainerBuilder $container
      */
